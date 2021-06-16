d426e529455e6eb81df2b5d32c228287f94390a7
renpu-mcarlo <renpu@mcarlo.com>
2021-06-16 19:21:30 +0800

    fix

--- src/route.ts
+++ src/route.ts
@@ -3,7 +3,7 @@ import { Gateway, DefaultGateway } from '../generated/schema';
 import {
   GatewaySet as GatewaySetEvent,
   DefaultGatewayUpdated as DefaultGatewayUpdatedEvent,
-} from '../generated/templates/Bridge';
+} from '../generated/mai3-arb-bridge/Route';
 
 import { 
     Bridge as BridgeTemplate,
@@ -14,9 +14,12 @@ export function handleGatewaySet(event: GatewaySetEvent): void {
   let entity = Gateway.load(id)
   if (entity === null) {
     entity = new Gateway(id)
+    entity.tokens = []
     BridgeTemplate.create(event.params.gateway)
   }
-  entity.token = event.params.l1Token.toHexString()
+  let tokens = entity.tokens
+  tokens.push(event.params.l1Token.toHexString())
+  entity.tokens = tokens
   entity.blockNumber = event.block.number
   entity.timestamp = event.block.timestamp
   entity.txHash = event.transaction.hash.toHex()
@@ -30,12 +33,12 @@ export function handleDefaultGatewayUpdated(event: DefaultGatewayUpdatedEvent): 
   let gatway = Gateway.load(gatewayID)
   if (gatway === null) {
     gatway = new Gateway(gatewayID)
-    gatway.token = ''
+    gatway.tokens = []
     gatway.blockNumber = event.block.number
     gatway.timestamp = event.block.timestamp
     gatway.txHash = event.transaction.hash.toHex()
     gatway.save()
-    BridgeTemplate.create(event.params.gateway)
+    BridgeTemplate.create(event.params.newDefaultGateway)
   }
 
   entity.gateway = gatewayID
