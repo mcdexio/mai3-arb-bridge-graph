c02488b6b65d410374043b3ffa353a43b618a89e
renpu-mcarlo <renpu@mcarlo.com>
2021-06-21 11:07:26 +0800

    update MessageDelivered

--- src/delay.ts
+++ src/delay.ts
@@ -5,6 +5,9 @@ import {
 } from '../generated/Delay/Delay';
 
 export function handleMessageDelivered(event: MessageDeliveredEvent): void {
+    if (event.transaction.value == BigInt.fromI32(0)) {
+        return
+    }
     let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
     let entity = new MessageDelivered(id)
     entity.messageIndex = event.params.messageIndex
@@ -14,5 +17,8 @@ export function handleMessageDelivered(event: MessageDeliveredEvent): void {
     entity.sender = event.params.sender.toHexString()
     entity.messageDataHash = event.params.messageDataHash.toHexString()
     entity.value = event.transaction.value
+    entity.blockNumber = event.block.number
+    entity.timestamp = event.block.timestamp
+    entity.txHash = event.transaction.hash.toHex()
     entity.save()
   }
